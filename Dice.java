4271159d84b7ce53f52d9f91546ff16b9dbcc3f6
Connor-Rosenberry <121972458+Connor-Rosenberry@users.noreply.github.com>
2025-02-07 12:42:40 -0800

    added role feature

--- Dice.java
+++ Dice.java
@@ -1,1 +1,13 @@
+public class Dice {
 
+    public int role() {
+        return (int) (Math.random() * (7 - 1) + 1);
+    }
+    
+    // public static void main(String args[]) {
+    //     Dice dice = new Dice();
+    //     int test;
+    //     test = dice.role();
+    //     System.out.println(test);
+    // }
+}
