386ebe164f732f5a230de390863bd57d38d0de3a
Hailey S <steffeh2@wwu.edu>
2025-02-10 11:07:44 -0800

    more scenecreator work + role

--- SceneCreator.java
+++ SceneCreator.java
@@ -7,7 +7,7 @@ import org.w3c.dom.NodeList;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
-// reads in xml file and creates all 40 scenes, setting them in the scene array
+// reads in xml file and creates all 40 scenes (including roles), setting them in the scene array
 public class SceneCreator {
     //  VARIABLES
     public Scene[] scenes;
@@ -40,7 +40,7 @@ public class SceneCreator {
             // get all card nodes
             NodeList nodeList = root.getElementsByTagName("card");
             
-            // iterate over employee nodes and store info
+            // for all scenes
             for (int i = 0; i < nodeList.getLength(); i++) {
                 Node node = nodeList.item(i);
                 if (node.getNodeType() == Node.ELEMENT_NODE) {
@@ -48,11 +48,11 @@ public class SceneCreator {
 
                     // card attributes
                     Element card = (Element) node;
-                    String name = card.getAttribute("name");
+                    String sceneName = card.getAttribute("name");
                     String img = card.getAttribute("img");
                     String budget = card.getAttribute("budget");
                     // assign attributes
-                    newScene.setName(name);
+                    newScene.setName(sceneName);
                     newScene.setImg(img);
                     newScene.setBudget(Integer.parseInt(budget));
 
@@ -65,6 +65,48 @@ public class SceneCreator {
                     newScene.setDescription(description);
 
                     // get role info
+                    NodeList partList = card.getElementsByTagName("part");
+                    Role[] roles = new Role[partList.getLength()];
+
+                    // for all parts
+                    for (int j = 0; j <partList.getLength(); j++) {
+                        Role newRole = new Role();
+
+                        // part attributes
+                        Element part = (Element) partList.item(j);
+                        String roleName = part.getAttribute("name");
+                        String level = part.getAttribute("level");
+
+                        // get dialogue
+                        Element line = (Element) part.getElementsByTagName("line").item(0);
+                        String dialogue = line.getTextContent().trim();
+
+                        // get area
+                        Element area = (Element) part.getElementsByTagName("area").item(0);
+                        String x = area.getAttribute("x");
+                        String y = area.getAttribute("y");
+                        String w = area.getAttribute("w");
+                        String h = area.getAttribute("h");
+
+                        // assign attributes
+                        newRole.setName(roleName);
+                        newRole.setRankToAct(Integer.parseInt(level));
+                        newRole.setStatus("not active");  // not on board yet
+                        newRole.setPriority(partList.getLength() - j);  // ordered in reverse priority
+                        newRole.setOnCard(true);  // all these roles are from cards
+                        newRole.setScene(newScene);  // set to current scene
+                        newRole.setDialogue(dialogue);
+                        newRole.setX(Integer.parseInt(x));
+                        newRole.setY(Integer.parseInt(y));
+                        newRole.setW(Integer.parseInt(w));
+                        newRole.setH(Integer.parseInt(h))
+
+                        // populate scenes roles array
+                        roles[j] = newRole;
+                    }
+
+                    // assign roles array to scene
+                    newScene.setRoles(roles);  // STOPPED HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
                 }
             }
             
