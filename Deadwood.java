7a1c4d14cbe5f08749ce2a22213a702e621fc430
Connor-Rosenberry <rosenbc4@wwu.edu>
2025-02-10 17:32:23 -0800

    Basic layout for the MVC architecture

--- Deadwood.java
+++ Deadwood.java
@@ -1,2 +1,13 @@
+public class Deadwood {
 
-//test 
+    public static void main(String[] args) {
+        // initialize the components of the game
+        Board board = new Board();
+        View view = new View();
+        Moderator moderator = new Moderator(board, view);
+
+        // start the game
+        view.startGame();
+        moderator.startGame();
+    }
+}
--- Moderator.java
+++ Moderator.java
@@ -1,1 +1,31 @@
+public class Moderator {
+    private Board board;
+    private View view;
+    private boolean gameRunning;
 
+    public Moderator(Board board, View view) {
+        this.board = board;
+        this.view = view;
+        this.gameRunning = true;
+    }
+
+    public void startGame() {
+        view.displayMessage("How many players will be playing today?");
+
+        while(gameRunning) {
+
+            // get the player and tell them its their turn
+            view.displayMessage("its player" + "'s turn");
+
+            String input = view.getUserInput();
+
+            handleInput(input);
+        }
+
+        view.displayMessage("Game Over");
+    }
+
+    private void handleInput(String input) {
+        
+    }
+}
